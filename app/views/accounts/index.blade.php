7b5a70e0aa81262debd097bbe06bdd8ad0ca74c2
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-06-13 18:17:34 +0530

    adding main page of account.**
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ app/views/accounts/index.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Accounts - Home</title>        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">        
+        {{ HTML::style('css/bootstrap.min.css') }}
+        {{ HTML::style('css/bootstrap-responsive.min.css') }}
+    </head>
+    <body>
+        
+    </body>
+</html>
