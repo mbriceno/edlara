26d86f5c128a2a2f1d6d0d31365976e59faa9e0e
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-06-13 18:15:11 +0530

    adding bootstrap css links
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,7 +1,10 @@
 <!doctype html>
 <html>
     <head>
-        <title>Edulara</title>
+        <title>Edulara</title>        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        {{ HTML::style('css/bootstrap.min.css') }}
+        {{ HTML::style('css/bootstrap-responsive.min.css') }}
     </head>
     <body>
         
