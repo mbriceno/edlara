841017866e1956a7ed1005b8febd7ad2ff009692
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-06-14 22:14:06 +0530

    adding statistics and dashboard view
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ app/views/dashboard/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Dashboard - Home</title>        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @stylesheets('bootstrap')
+    </head>
+    <body>
+        
+        
+        {{-- Bootstrap JS Compiled --}}
+        @javascripts('bootstrap')
+    </body>
+</html>
---
+++ app/views/statistics/index.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Statistics - Home</title>        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            @stylesheets('bootstrap')
+    </head>
+    <body>
+        
+    </body>
+</html>
