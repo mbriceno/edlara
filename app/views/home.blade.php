22397c9820ce324867b329ae7475c118a9539063
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-06-15 13:47:27 +0530

    removing some lines of code
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -9,35 +9,25 @@
     <body>
         <div class="container-fluid" id='top-heading'>
             <div class="row-fluid" >
-                <!--
-So we will have an animated background with 5 clouds moving across the screen.
-Steps: 
-1. create the clouds
-2. Animate them to move across the screen
-3. Stylize the clouds(can be done as step #2 also)
--->
-<div id="clouds">
-	<div class="cloud x1"></div>
-	<!-- Time for multiple clouds to dance around -->
-	<div class="cloud x2"></div>
-	<div class="cloud x3"></div>
-	<div class="cloud x4"></div>
-	<div class="cloud x5"></div>
-</div>
-<!-- Thats the markup! -->
-<!-- That looks cool. We are done!! -->
+                <div id="clouds">
+                    <div class="cloud x1"></div>
+                    <div class="cloud x2"></div>
+                    <div class="cloud x3"></div>
+                    <div class="cloud x4"></div>
+                    <div class="cloud x5"></div>
+                </div>
                 <span class="brand-name" id='top-header'>
                     EdLara
                 </span>
             </div>
-            
+
         </div>
 
         {{-- Bootstrap JS Compiled --}}
         @javascripts('bootstrap')
         @javascripts('grans')
         <script type="text/javascript">
-       // $("#top-header").fitText(1.0, { minFontSize: '24px', maxFontSize: '480px' });
+            // $("#top-header").fitText(1.0, { minFontSize: '24px', maxFontSize: '480px' });
         </script>
     </body>
 </html>
