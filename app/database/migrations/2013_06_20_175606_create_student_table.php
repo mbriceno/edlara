5889e693f36ccb10e89d29866cb9a7007726ce01
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-06-20 19:56:34 +0530

    migration classes setup
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ app/database/migrations/2013_06_20_175606_create_student_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('student', function(Blueprint $table) {
+            $table->increments('id');
+            $table->
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('student');		
+	}
+
+}
