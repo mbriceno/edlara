46572eac1ec98ef082d91d2d1d8166997ecc637b
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-06-22 14:38:13 +0530

    login page set
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/views/account/login.blade.php
+++ app/views/account/login.blade.php
@@ -1,10 +1,26 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Login - EdLara</title>   
+        <title>Edlara -Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @stylesheets('bootstrap')
+        @stylesheets('grans')
     </head>
     <body>
         <h1>Login Page</h1>
+
+        {{ Form::open(array('url' => 'login','method'=>'post')) }}
+
+        {{ Form::text('email', 'example@gmail.com');}}
+        {{ Form::password('password') }}
+        {{ Form::submit('Click Me!') }}
+        {{--TOKEN FOR THE FORM--}}
+        {{ Form::token() }}
+        {{ Form::close() }}
+
+        {{-- Bootstrap JS Compiled --}}
+        @javascripts('bootstrap')
+        @javascripts('grans')
     </body>
-    
+
 </html>
