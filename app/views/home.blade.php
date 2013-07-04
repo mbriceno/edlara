e28cc64e9ae7c6e8bf4b938bfcb2b15598165327
gnanakeethan <gnanakeethan@gmail.com>
2013-07-04 12:50:15 +0530

    using blade call to include view from view composer.
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -7,19 +7,7 @@
         @stylesheets('grans')
     </head>
     <body>
-        <div class="container-fluid" id='top-heading'>
-            <div class="row-fluid" >
-                <div id="clouds">
-                    <div class="cloud x1"></div>
-                    <div class="cloud x2"></div>
-                    <div class="cloud x3"></div>
-                    <div class="cloud x4"></div>
-                    <div class="cloud x5"></div>
-                </div>
-                <span class="brand-name" id='top-header'>EdLara</span>
-            </div>
-        </div>
-        {{ $menubar }}
+        {{ $header }}
         <div class='container-fluid'>
             <div class='row-fluid'>
                 <div class=span4>
@@ -29,5 +17,8 @@
         {{-- Bootstrap JS Compiled --}}
         @javascripts('bootstrap')
         @javascripts('grans')
+        <script type="text/javascript">
+            $('#navbar').scrollspy();
+        </script>
     </body>
 </html>
