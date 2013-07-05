ade38865ae91d44bc74153509203ce7b77280777
gnanakeethan <gnanakeethan@gmail.com>
2013-07-05 20:04:36 +0530

    creating workbench package for Madango ODM
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

---
+++ workbench/gnanakeethan/ladango/src/Gnanakeethan/Ladango/LadangoServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Gnanakeethan\Ladango;
+
+use Illuminate\Support\ServiceProvider;
+
+class LadangoServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('gnanakeethan/ladango');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
