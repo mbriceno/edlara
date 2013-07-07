10ab505daec63a39b0806bd5e781af2ac3873f49
gnanakeethan <gnanakeethan@gmail.com>
2013-07-07 16:09:42 +0530

    some changes to mailer controller
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

--- app/controllers/MailerController.php
+++ app/controllers/MailerController.php
@@ -1,19 +1,22 @@
 <?php
 
 class MailerController extends BaseController {
-    public function welcomeMail(){
-        $activation_code = Session::get('user.activationcode');
+    public function welcomeMail()
+    {
+        $activation_code = \Session::get('user.activationcode');
         $fname = $this->app('Input')->get('fname');
         $lname = $this->app('Input')->get('lname');
         $email = $this->app('Input')->get('email');
         $data = ['activation_code'=>$activation_code,
                     'fname'=> $fname,
                     'lname'=>$lname,
-                    'email'=>$email];
-        Mail::queue('emails.welcome',$data,function($message)
+                    'email'=>$email,
+                    'fullname'=>$fname.' '.$lname];
+        Mail::queue('emails.welcome',$data,function($message) use ($data)
         {
-            $message->to($email,$fname.' '.$lname)->subject('Welcome! to EdLara');
+            $message->to($data['email'],$data['fullname'])->subject('Welcome! to EdLara');
         });
+        return true;
     }   
 
 }
