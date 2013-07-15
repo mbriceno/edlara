ba374811b528452c99f1faa7eb962c5c3e48ae17
gnanakeethan <gnanakeethan@gmail.com>
2013-07-15 20:49:34 +0530

    adding CDN bootstrap code link.
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

--- app/views/emails/welcome.php
+++ app/views/emails/welcome.php
@@ -3,15 +3,18 @@
     <head>
         <meta charset="utf-8">
         <title>
-            Welcome to Edlara Educational System.
+            Welcome to Edlara Educational System
         </title>
-        <style type="text/css">        
+        <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet"/>
+        <style type="text/css">  
+        @import url(http://fonts.googleapis.com/css?family=Metamorphous);
         h2#heading{
-            font:Metamorphus;
+            font-family:'Metamorphous';
             color: #ccddss;
         }
         </style>
     </head>
     <body>        
+<h2 id="heading">Test</h2>
     </body>
 </html>
