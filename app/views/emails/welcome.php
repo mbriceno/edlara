6d96a0ed825ff8bf264a7bb45127b80a34d19a2e
gnanakeethan <gnanakeethan@gmail.com>
2013-07-15 05:35:24 +0530

    re-adding welcome.php file.
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

---
+++ app/views/emails/welcome.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Welcome to Edlara Educational System.
+        </title>
+        <style type="text/css">        
+        h2#heading{
+            font:Metamorphus;
+            color: #ccddss;
+        }
+        </style>
+    </head>
+    <body>        
+    </body>
+</html>
