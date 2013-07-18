7577452fa700ea27f2406687ee0f8bccafa65736
gnanakeethan <gnanakeethan@gmail.com>
2013-07-18 07:30:08 +0530

    adding about us page.
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

---
+++ app/views/about/about-us.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Edlara - About Us</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @stylesheets('bootstrap')
+        @stylesheets('grans')
+    </head>
+    <body>
+        {{ $header }}
+        <div class='container-fluid'>
+            <div class='row-fluid'>
+                <div class=span8>
+                	
+                </div>
+            </div>
+        </div>
+        {{-- Bootstrap JS Compiled --}}
+        @javascripts('bootstrap')
+        @javascripts('grans')
+    </body>
+</html>
