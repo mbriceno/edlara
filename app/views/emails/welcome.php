ea1db5080725b8e9dd189143be068bb9361de857
gnanakeethan <gnanakeethan@gmail.com>
2013-08-08 10:23:56 +0530

    adding activation code to welcome email.
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

--- app/views/emails/welcome.php
+++ app/views/emails/welcome.php
@@ -5,7 +5,9 @@
         <title>
             Welcome to Edlara Educational System.
         </title>
-        <style type="text/css">        
+        <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet"/>
+        <style type="text/css">  
+        @import url(//fonts.googleapis.com/css?family=Metamorphous);
         h2#heading{
             font:Metamorphus;
             color: #ccddss;
@@ -13,7 +15,7 @@
         </style>
     </head>
     <body>
-    <h2>Welcome to Edlara Educational System.</h2>
+    <h2 id="heading">Welcome to Edlara Educational System.</h2>
 
     Hi <?php echo $fname.' '.$lname ?>,<br>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You have recently joined in our Online Educational Programme.
@@ -24,8 +26,17 @@ This educational programme provides you with multiple types of exams. Such as
 </ul>
 You can contact us at anytime using this email address<a href="mailto:info@edlaraedu.com">info@edlara.com</a>
 <br>
-
-<br><p style="font-size:10px;">If You have not registered , it may be due to a typo error. Sorry for any inconveinience caused.</p>
+<?php 
+$url =  Config::get('app.url', 'https://laravel.dev/');
+echo "<a href='".$url."activateuser/".$activation_code.'/'.$email."'>Click Here to Activate</a>";
+?>
+<p>Or</p>
+<p>Copy and Paste following URL in Browser</p>
+<?php 
+$url =  Config::get('app.url', 'https://laravel.dev/');
+echo $url."activateuser/".$activation_code.'/'.$email;
+?>
+<br><p style="font-size:10px;">If You have not registered , it may be due to a typing error. Sorry for any inconveinience caused.</p>
 <p style="align:right;">
     Thanks,<br>
     Edlara Team</p>
