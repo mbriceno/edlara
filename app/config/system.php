24c638deda9dc14d8c548d59b861446764c9ca2c
gnanakeethan <gnanakeethan@gmail.com>
2013-08-12 07:08:21 +0530

    moving charisma files to general directories
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

---
+++ app/config/system.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ | Here the settings for the System such as Logo/ Name of School are set.
+ |
+ |
+ |
+ |
+ |
+ |
+ */
+
+
+return array(
+
+
+    "schoolname"=>"Jaffna Hindu College",
+
+    "schooladdress"=>"Jaffna",
+
+    "logo_src" => "img/logo.png",
+
+
+);
