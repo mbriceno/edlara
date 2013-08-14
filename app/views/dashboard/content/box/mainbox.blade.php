64843db490f1ceec2ee1529ca3c6c6072f8cfc67
gnanakeethan <gnanakeethan@gmail.com>
2013-08-14 06:44:16 +0530

    content box fixer.
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

---
+++ app/views/dashboard/content/box/mainbox.blade.php
@@ -0,0 +1,14 @@
+    <div class="row-fluid sortable">
+                
+            {{$contentbox1}}
+            {{$contentbox2}}
+            {{$contentbox3}}
+            </div><!--/row-->
+
+            <div class="row-fluid sortable">
+                
+            {{$contentbox4}}
+            {{$contentbox5}}
+            {{$contentbox6}}                 
+               
+            </div><!--/row-->
