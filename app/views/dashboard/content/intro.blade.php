14e8512148cd88da72736a10b666836fe1fded2f
gnanakeethan <gnanakeethan@gmail.com>
2013-08-14 06:43:38 +0530

    dashboard introduction
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

---
+++ app/views/dashboard/content/intro.blade.php
@@ -0,0 +1,16 @@
+            <div class="row-fluid">
+                <div class="box span12">
+                    <div class="box-header well">
+                        <h2><i class="icon-info-sign"></i> Introduction</h2>
+                        <div class="box-icon">
+                            <a href="#" class="btn btn-setting btn-round"><i class="icon-cog"></i></a>
+                            <a href="#" class="btn btn-minimize btn-round"><i class="icon-chevron-up"></i></a>
+                            <a href="#" class="btn btn-close btn-round"><i class="icon-remove"></i></a>
+                        </div>
+                    </div>
+                    <div class="box-content">
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+                    
