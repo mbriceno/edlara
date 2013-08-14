00ca19a1fbe4567a15539c7660f3ee20c1cb281c
gnanakeethan <gnanakeethan@gmail.com>
2013-08-14 06:46:09 +0530

    adding breadcrumbs code.
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

---
+++ app/views/dashboard/breadcrumbs/top.blade.php
@@ -0,0 +1,8 @@
+ <ul class="breadcrumb">
+                    <li>
+                        <a href="#">Home</a> <span class="divider">/</span>
+                    </li>
+                    <li>
+                        <a href="#">Dashboard</a>
+                    </li>
+</ul>
