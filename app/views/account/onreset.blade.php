25cc75724b3d0793c7bdfecaa54f5055daf97d34
gnanakeethan <gnanakeethan@gmail.com>
2013-08-31 09:42:08 +0530

    page to show after reset created.:cook:
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

---
+++ app/views/account/onreset.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Edlara - ResetPassword</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @stylesheets('bootstrap')
+        @stylesheets('grans')      
+    </head>
+    <body>         
+        <div class='container-fluid'>
+            <div class='row-fluid'>                
+                <div id="pwd-reset-form" class="span-4 offset4">                    
+                    Password has been reset. you can now login with your new password.
+                    <a class="btn btn-large btn-danger" href="/gohome">Return to HomePage</a>
+                </div>
+            </div>
+        </div>
+        
+        {{-- Bootstrap JS Compiled --}}
+        
+         @javascripts('bootstrap')
+         @javascripts('grans') 
+        
+    </body>
+
+</html>
