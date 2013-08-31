f6e60f1d02631306e6ca10a1a4743315d3f98a7f
gnanakeethan <gnanakeethan@gmail.com>
2013-08-31 06:27:10 +0530

    activating type based accounts. TODO: adding to user table.
    Signed-off-by: gnanakeethan <gnanakeethan@gmail.com>

--- app/views/account/register.blade.php
+++ app/views/account/register.blade.php
@@ -117,7 +117,7 @@
 
 
                         echo "<div class='span4'>";
-                        echo Form::select('actype', array('S' => 'Student', 'T' => 'Teacher'), 'S',array('class'=>'actype-reg-box','name'=>'actype'));
+                        echo Form::select('actype', array('S' => 'student', 'T' => 'teacher'), 'S',array('class'=>'actype-reg-box','name'=>'actype'));
                         echo "</div></div><br>* Required<br><br>";
 
 
@@ -170,6 +170,9 @@
                         required: true,
                         minlength: 5
                     },
+                    actype:{
+                        required:true;
+                    }
                     password:{
                         required: true,
                         minlength: 8,
