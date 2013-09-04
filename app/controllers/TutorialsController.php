a9015daa719bb32240b9b3a60fd25df2921897ff
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-04 16:57:46 +0530

    adding tutorials controller functions.

--- app/controllers/TutorialsController.php
+++ app/controllers/TutorialsController.php
@@ -3,14 +3,48 @@
 class TutorialsController extends BaseController {
     public function index($id){
         if($id == 0){
-            return View::make('dashboard.tutorials.create');
+            return View::make('dashboard.tutorials.create')->with('id',$id);
         }
         else{
             return View::make('dashboard.tutorials.edit')->with('id',$id);
         }
     }
 
-    public function update(){
-        
+    public function update($id){
+        if($id == 0){
+            self::newtutorial($id);
+        }
+        else {
+            self::updatetutorial($id);
+        }
     }
+
+    private function newtutorial($id){
+        $validator = Validator::make(Input::all(),
+                            array('id'=>'required',
+                                  'title'=>'required|min:3|max:256|alpha',
+                                  'description'=> 'max:1024|alpha',
+                                  'content'=>'required',
+                                  'attachment'=>'mime:jpeg,png,bmp,pdf|size:10240'
+                                ));
+        $messages = array(
+            'required' => 'The :attribute field is required.',
+        );
+        if ($validator->fails())
+        {           
+            Input::flash();
+            return Redirect::to('tutorial/edit/'.$id)->withErrors($validator);
+        } 
+        else
+        {              
+                return Redirect::to('/');
+        }
+        return;
+    }
+    private function updatetutorial($id){
+
+        return;
+    }
+
+
 }
