bbaeb4ef17aae9363c5ebee097b3fee839731318
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-15 20:22:50 +0530

    adding student table view
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/views/dashboard/students.blade.php
+++ app/views/dashboard/students.blade.php
@@ -33,9 +33,67 @@
             <div>
         {{$breadcrumbs}}
             </div>
-            <div id="test">
-            
-            </div>
+             <table class="table table-striped table-bordered bootstrap-datatable datatable">
+                <thead>
+                    <tr>
+                        <th>#ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Date of Birth</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+            <?php
+                $students = Student::all();
+                foreach ($students as $student){
+                    $user = Sentry::findUserByLogin($student->email);
+                    echo "<tr>";
+                    echo "<td>";
+                    echo $student->id;
+                    echo "</td>";
+                    echo "<td>";
+                    echo $user->first_name;
+                    echo "</td>";
+                    echo "<td>";
+                    echo $user->last_name;
+                    echo "</td>";
+                    echo "<td>";
+                    echo $student->dob;
+                    echo "</td>";
+                    echo "<td class='center'>";
+                                if($user->activated){
+                                    echo "<span class='label label-success'>Activated</span>";
+                                }
+                                else
+                                {
+                                    echo "<span class='label label-failure'>Not Activated</span>";
+                                }
+                                echo "</td>";
+                                echo '<td class="center">
+                                    <a class="btn btn-success" href="/user/'.$user->id.'/view">
+                                        <i class="icon-zoom-in icon-white"></i>  
+                                        View                                            
+                                    </a>
+                                    <a class="btn btn-info" href="/user/'.$user->id.'/edit">
+                                        <i class="icon-edit icon-white"></i>  
+                                        Edit                                            
+                                    </a>
+                                    <a class="btn btn-danger" href="/user/'.$user->id.'/delete">
+                                        <i class="icon-trash icon-white"></i> 
+                                        Delete
+                                    </a>
+                                    </td>';
+                    echo "</tr>";
+                    
+                }
+
+
+
+            ?>
+                    </tbody>
+                </table>
             <!-- content ends -->
             </div><!--/#content.span10-->
         </div><!--/fluid-row-->
