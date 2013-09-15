fa402c93251c3856aa1be6974e3a202ee271e329
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-15 11:12:52 +0530

    including published option on create tutorial page.
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/views/dashboard/tutorials/create.blade.php
+++ app/views/dashboard/tutorials/create.blade.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="utf-8">
     <title>{{ Config::get('system.sitename') }}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<base href="https://laravel.dev/">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <base href="https://dashboard.laravel.dev/"/>
 
     <!-- The styles -->
     <style type="text/css">      
@@ -14,22 +13,7 @@
     <link id="bs-css" href="https://laravel.dev/css/bootstrap-cerulean.css" rel="stylesheet">
     <style type="text/css">      
     </style>
-    <link href="/css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="/css/charisma-app.css" rel="stylesheet">
-    <link href="/css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
-    <link href='/css/fullcalendar.css' rel='stylesheet'>
-    <link href='/css/fullcalendar.print.css' rel='stylesheet'  media='print'>
-    <link href='/css/chosen.css' rel='stylesheet'>
-    <link href='/css/uniform.default.css' rel='stylesheet'>
-    <link href='/css/colorbox.css' rel='stylesheet'>
-    <link href='/css/jquery.cleditor.css' rel='stylesheet'>
-    <link href='/css/jquery.noty.css' rel='stylesheet'>
-    <link href='/css/noty_theme_default.css' rel='stylesheet'>
-    <link href='/css/elfinder.min.css' rel='stylesheet'>
-    <link href='/css/elfinder.theme.css' rel='stylesheet'>
-    <link href='/css/jquery.iphone.toggle.css' rel='stylesheet'>
-    <link href='/css/opa-icons.css' rel='stylesheet'>
-    <link href='/css/uploadify.css' rel='stylesheet'>
+    @stylesheets('dashboard')
 
 
     <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -62,7 +46,7 @@
                         <div class="row-fluid">
                             <div class="span6">
                             <?php
-
+                            var_dump($errors);
                             echo Form::open(array('url' => '/tutorial/edit/'.$id.'/update', 'method' => 'post','class'=>'form-horizontal'));
                             echo "<fieldset>";
                             echo Form::label('id','id',array('class'=>'pull-left','style'=>'clear:left;padding:15px;'));
@@ -76,7 +60,8 @@
                             echo Form::label('description','Description',array('class'=>'pull-left','style'=>'clear:left;margin:15px;'));
 
                             echo Form::text("description","",array('placeholder'=>'Describe the Tutorial Here','class'=>'pull-right','style'=>'clear:right;margin:10px;'));
-
+                            echo Form::label('published','Published',array('class'=>'pull-left','style'=>'clear:left;margin:15px;'));
+                            echo '<span  class="pull-right" style="clear:right;margin:15px;"><input id="published" name="published" data-no-uniform="true" type="checkbox"  class="iphone-toggle"></span>';
                             echo '
                             <div class="control-group" style="clear:left;">';
                             echo Form::label('tutorial',"Tutorial Content",array('class'=>'pull-left control-label','style'=>''));
@@ -88,12 +73,14 @@
                             echo Form::file('attachment', array('class'=>"pull-right",'style'=>'clear:right;margin:25px;'));
                             
                             echo "</fieldset>";
-                            echo Form::submit('Create',array('class'=>'','value'=>'submit'));
+                            echo Form::submit('Create',array('class'=>'btn btn-success','value'=>'submit'));
+                            echo '
+                            <a class="btn btn-danger" href="/tutorials">Cancel</a>';
                             echo Form::close();
                             ?>
                             </div>
                             <div class="offset2 span3">
-                                DISPLAY
+
                             </div>
                         </div>
                     </div>
