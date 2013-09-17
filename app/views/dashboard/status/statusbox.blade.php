13b8fc4e7a4f9885922d7f1a2427a2d85ee21375
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-17 21:04:58 +0530

    issue solved
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/views/dashboard/status/statusbox.blade.php
+++ app/views/dashboard/status/statusbox.blade.php
@@ -1,13 +1,43 @@
 <?php
-
-$todayusercount = User::where('created_at', '=', 'CURDATE()')->count();
+$studentquery = DB::select(DB::raw('SELECT COUNT(`dob`) FROM `students` WHERE `created_at` >= CURDATE() - INTERVAL 1 DAY'));
+$userquery = DB::select(DB::raw('SELECT COUNT(`email`) FROM `users` WHERE `created_at` >= CURDATE() - INTERVAL 1 DAY'));
+$tutorialquery = DB::select(DB::raw('SELECT COUNT(`id`) FROM `tutorials` WHERE `created_at` >= CURDATE() - INTERVAL 1 DAY'));
+function objectToArray($d) {
+        if (is_object($d)) {
+            // Gets the properties of the given object
+            // with get_object_vars function
+            $d = get_object_vars($d);
+        }
+ 
+        if (is_array($d)) {
+            /*
+            * Return array converted to object
+            * Using __FUNCTION__ (Magic constant)
+            * for recursive call
+            */
+            return array_map(__FUNCTION__, $d);
+        }
+        else {
+            // Return array
+            return $d;
+        }
+    }
+$stresult = objectToArray($studentquery);
+$usresult = objectToArray($userquery);
+$turesult = objectToArray($tutorialquery);
+$todayusercount = $usresult[0]['COUNT(`email`)'];
 $usercountall = User::all()->count();
 $usercountall -= 1;
-$todaystudentcount = Student::where('created_at', '=', 'CURDATE()')->count();
+$todaystudentcount = $stresult[0]['COUNT(`dob`)'];
 $studentcountall = Student::all()->count();
 $studentcountall -=1;
-?>
+$todaytutorialcount = $turesult[0]['COUNT(`id`)'];;
+$tutorialcountall = Tutorials::all()->count();
 
+?>
+<?php 
+
+ ?>
 <div class="sortable row-fluid">
                 <a data-rel="tooltip" title="{{ $todayusercount }} new members." class="well span3 top-block" href="#">
                     <span class="icon32 icon-red icon-user"></span>
@@ -23,17 +53,17 @@ $studentcountall -=1;
                     <span class="notification blue">{{ $todaystudentcount }}</span>
                 </a>
 
-                <a data-rel="tooltip" title="$34 new sales." class="well span3 top-block" href="#">
-                    <span class="icon32 icon-color icon-cart"></span>
-                    <div>Sales</div>
-                    <div>$13320</div>
-                    <span class="notification yellow">$34</span>
+                <a data-rel="tooltip" title="{{$todaytutorialcount }} New Tutorials Today" class="well span3 top-block" href="#">
+                    <span class="icon32 icon-page icon-green"></span>
+                    <div>Tutorials</div>
+                    <div>{{$tutorialcountall }}</div>
+                    <span class="notification green">{{$todaytutorialcount }}</span>
                 </a>
                 
                 <a data-rel="tooltip" title="12 new messages." class="well span3 top-block" href="#">
                     <span class="icon32 icon-color icon-envelope-closed"></span>
-                    <div>Messages</div>
-                    <div>25</div>
+                    <div>Assessments Submitted</div>
+                    <div></div>
                     <span class="notification red">12</span>
                 </a>
 </div>
