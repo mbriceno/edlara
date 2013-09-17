d08b8a557c3f2bebeaa9c62106d2727faa8d03d5
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-17 21:16:18 +0530

    creating model and migration for assessments
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ app/database/migrations/2013_09_17_190221_create_assessments_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateAssessmentsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assessments', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 64);
+			$table->text('description', 255);
+			$table->string('assessmenttype', 64);
+            $table->integer('tutorialid');
+            $table->integer('teacherid');
+            $table->integer('studentid');
+            $table->text('groupstudentid');
+			$table->string('subjectcode', 64);
+			$table->text('attachments');
+            $table->integer('marks');
+            $table->text('result');
+            $table->timestamps();                       
+            $table->softDeletes();
+        });
+        //Setting extra data to LongText to allow much data to be put in
+        DB::statement('ALTER TABLE `assessments`  MODIFY `attachments` LONGTEXT;');
+        DB::statement('ALTER TABLE `assessments`  MODIFY `result` LONGTEXT;');
+        DB::statement('ALTER TABLE `assessments`  MODIFY `groupstudentid` LONGTEXT;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('assessments');
+    }
+
+}
---
+++ app/models/Assessments.php
@@ -0,0 +1,7 @@
+<?php
+
+class Assessments extends Eloquent {
+    protected $guarded = array();
+
+    public static $rules = array();
+}
