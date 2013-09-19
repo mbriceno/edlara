38ac34890e61cff4332937d32f9f44ac35343567
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-19 11:49:10 +0530

    updating routes for profile.
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/routes.php
+++ app/routes.php
@@ -48,10 +48,6 @@ Route::group(array('domain' => 'dashboard.laravel.dev'), function()
     {
         return View::make('dashboard.tutorials');
     }));
-    Route::get('assessments',array('before'=>'teacher',function()
-    {
-        return View::make('dashboard.assessments');
-    }));
     Route::get('students',array('before'=>'teacher',function(){
         return View::make('dashboard.students');
     }));
@@ -67,6 +63,13 @@ Route::group(array('domain' => 'dashboard.laravel.dev'), function()
 
     Route::get('tutorial/update/{id}/{attachmentname}/{mode}',array('before'=>'teacher','uses'=>'TutorialsController@attachmentHandler'));
 
+
+
+
+    Route::get('assessments',array('before'=>'teacher',function()
+    {
+        return View::make('dashboard.assessments');
+    }));
 
     Route::get('/',array('before'=>'teacher','as'=>'dashboard',function()
     {
@@ -147,9 +150,12 @@ Route::group([],function(){
 
 
 Route::group([],function(){
-    Route::get('u/{username}.html','UserController@showProfile');
+    Route::get('profiles',function(){
+        return;
+    });
+    Route::get('profile/{id?}','UserController@showProfile')->where('id', '[0-9]+');
 
-    Route::get('u/{username}.html?edit=true',array('uses'=>'UserController@editProfile','before'=>'auth'));
+    Route::get('profile/{id?}?edit=true',array('uses'=>'UserController@editProfile','before'=>'auth'));
 
 });
 
@@ -184,6 +190,12 @@ Route::get('gohome',function(){
 Route::get('dash',function(){
     return Redirect::route('dashboard');
 });
+
+
+//Tutorials
+Route::get('/tutorial/{id}',array('uses'=>'TutorialsController@siteitemview'));
+Route::get('/tutorials',array('uses'=>'TutorialsController@sitelistview'));
+
 
 //HomePage Catcher
 Route::get('/',array('as'=>'home',function()
