41b4cb8510d30f3b24b0018908b1163225bbc240
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-19 12:27:03 +0530

    correcting typo mistake on Controller
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ app/controllers/AssessmentController.php
@@ -0,0 +1,26 @@
+<?php
+
+class AssessmentController extends BaseController {
+
+	public function siteview($id){
+		return View::make('dashboard.assessments.view');
+	}
+	public function dashview($id){
+		return;
+	}
+	public function submit(){
+		return;
+	}
+	public function submitview(){
+		return;
+	}
+	public function update(){
+		return;
+	}
+	public function updateview(){
+		return;		
+	}
+	public function updatelist(){
+		return;
+	}
+}
