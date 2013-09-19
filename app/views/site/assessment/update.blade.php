5f7d6c3fb73167c80ce35fca07fb818d03dbf42a
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-19 21:49:00 +0530

    view for update assessment.
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ app/views/site/assessment/update.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+    <head>
+        <title>{{ Setting::get('system.schoolname') }}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @stylesheets('bootstrap')
+        @stylesheets('grans')
+    </head>
+    <body>
+        {{ $header }}
+        <div class='container-fluid'>
+            <div class='row-fluid'>
+                <div class="span8 offset2">
+                    <?php
+                    
+
+                    ?>
+                </div>
+            </div>
+        </div>
+        {{-- Bootstrap JS Compiled --}}
+        @javascripts('bootstrap')
+        @javascripts('grans')
+
+    <script src='/js/jquery.dataTables.min.js'></script>
+        <script type="text/javascript">
+            $('#navbar').scrollspy();
+        </script>
+    </body>
+</html>
