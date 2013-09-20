ae465a036c02949cf4cb0090df4beb3d1ca106e0
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-20 18:32:57 +0530

    deploy database and compose command update
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
-			'database'  => 'laravel',
-			'username'  => 'root',
-			'password'  => 'admin',
+			'host'      => 'localhost',
+			'database'  => 'tamilre1_edlara',
+			'username'  => 'tamilre1_edlara',
+			'password'  => 'SnaH&COe?ddK',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
