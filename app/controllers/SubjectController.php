8976bf2f6b0763637c40a7de6be10597a7f95606
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-21 10:50:04 +0530

    adding model to subject controllers for update, create
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/controllers/SubjectController.php
+++ app/controllers/SubjectController.php
@@ -1,6 +1,10 @@
 <?php
 
 class SubjectController extends BaseController {
+	function __construct() {
+      $this->beforeFilter('csrf', array('on' => 'update'));
+      $this->beforeFilter('csrf', array('on' => 'create'));
+    }
 	public function modder($id,$mode){
 		if(Request::getMethod() == 'GET'){
 			switch ($mode) {
@@ -40,16 +44,29 @@ class SubjectController extends BaseController {
 					self::update($id);
 					return Redirect::to(URL::previous());
 				case 'create':
-					self::create();
+					if($id == 0){
+					self::create();					
+					return Redirect::to(URL::previous());
+					}					
 					return Redirect::to(URL::previous());
 				default:
 					return "UNAUTHORISED METHOD";
 			}
 		}
 	}
-	private function update($id){		
+	private function update($id){	
+		$subject = Subject::findOrFail($id);
+		$subject->subjectname = Input::get('name');
+		$subject->subjectcode = Input::get('code');
+		$subject->grade 	  = Input::get('grade');
+		$subject->save();	
 	}
 	private function create(){
+		$subject = new Subject;
+		$subject->subjectname = Input::get('name');
+		$subject->subjectcode = Input::get('code');
+		$subject->grade 	  = Input::get('grade');
+		$subject->save();
 	}
 	private function delete($id){
 		$subject =  Subject::find($id);
