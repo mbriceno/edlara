b6cbc01ecc8368aa28a47be3721007b02c545942
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-22 11:54:05 +0530

    changes to student and teacher table seeder
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/database/seeds/StudentTableSeeder.php
+++ app/database/seeds/StudentTableSeeder.php
@@ -3,8 +3,8 @@
 class StudentTableSeeder extends Seeder {
     public function run() {
 
-        DB::table('students') -> delete();
-        $extra = array('grade' => '12', 'stream' => 'Maths', 'choices' => array('CM', 'P', 'C'), );
+        DB::table('students') -> delete();        
+        $extra = array('subjects'=>array('1','2','3','4'));
         $extra = serialize($extra);
         Student::create(array('user_id' => 1,'username'=>'johndoe', 'email' => 'johndoe@example.com', 'dob' => '1996-01-01', 'extra' => $extra));
     }
