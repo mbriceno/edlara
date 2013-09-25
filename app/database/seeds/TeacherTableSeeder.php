e84b2e6df16f4022f3842771e35d74b59ecaa9cc
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-25 14:27:37 +0530

    updating table seeders
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/database/seeds/TeacherTableSeeder.php
+++ app/database/seeds/TeacherTableSeeder.php
@@ -4,7 +4,7 @@ class TeacherTableSeeder extends Seeder {
     public function run() {
 
         DB::table('teachers')->delete();
-        $extra = array('subjects'=>array('1','2','3','4'));
+        $extra = array('1','2','3','4');
         $extra = serialize($extra);
         Teacher::create(array('user_id' => 1, 'email' => 'johndoe@example.com', 'dob' => '1996-01-01', 'extra' => $extra));
     }
