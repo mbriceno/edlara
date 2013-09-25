1229070d8f7c6ecf80d51e0de154ac67916faa87
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-25 23:29:34 +0530

    adding dashboard exams view.
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/viewcomposer.php
+++ app/viewcomposer.php
@@ -240,3 +240,14 @@ View::composer('dashboard.exams.edit',function($view)
     $view->nest('noscript','dashboard.warnings.noscript');
     return $view;
 });
+View::composer('dashboard.exams.view',function($view)
+{
+    $view->nest('breadcrumbs','dashboard.breadcrumbs.top');
+    $view->nest('topmenu','dashboard.menubar.menu');
+    $view->nest('sidebar','dashboard.sidebar.sidebar');
+    $view->nest('intro','dashboard.content.intro');
+    $view->nest('content','dashboard.content.index');
+    $view->nest('status','dashboard.status.statusbox');
+    $view->nest('noscript','dashboard.warnings.noscript');
+    return $view;
+});
