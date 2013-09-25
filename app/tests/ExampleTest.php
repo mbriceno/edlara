324de1c9b5e0dc244730f6d76ac9248b6462ece1
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-25 13:29:25 +0530

    Revert "Merge branch 'origin/dev-1.0.0'"
    This reverts commit f89b9a5669f89ba250842ae8b91f810eb9f32056, reversing
    changes made to 917043725bc75cadcdd69d7400cfa9f0a0b65d09.

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -13,6 +13,7 @@ class ExampleTest extends TestCase {
 
 		$this->assertTrue($this->client->getResponse()->isOk());
 
+        $this->assertCount(1, $crawler->filter('title:contains("Edulara")'));
 	}
 
 }
