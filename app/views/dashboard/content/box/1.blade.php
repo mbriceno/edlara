29753ef682b664ae70d25b65933210bedc437315
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-09-27 07:32:08 +0530

    updating content boxes.
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/views/dashboard/content/box/1.blade.php
+++ app/views/dashboard/content/box/1.blade.php
@@ -9,25 +9,13 @@
                     <div class="box-content">
                         <ul class="nav nav-tabs" id="myTab">
                             <li class="active"><a href="#info">Info</a></li>
-                            <li><a href="#custom">Custom</a></li>
-                            <li><a href="#messages">Messages</a></li>
                         </ul>
                          
                         <div id="myTabContent" class="tab-content">
                             <div class="tab-pane active" id="info">
-                                <h3>Charisma <small>a fully featued template</small></h3>
-                                <p>Its a fully featured, responsive template for your admin panel. Its optimized for tablet and mobile phones. Scan the QR code below to view it in your mobile device.</p> <img alt="QR Code" class="charisma_qr center" src="img/qrcode136.png" />
+                               This is a System Developed for Codefest 2013
                             </div>
-                            <div class="tab-pane" id="custom">
-                                <h3>Custom <small>small text</small></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare dolor.</p>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare dolor, quis ullamcorper ligula sodales at. Nulla tellus elit, varius non commodo eget, mattis vel eros. In sed ornare nulla. Donec consectetur, velit a pharetra ultricies, diam lorem lacinia risus, ac commodo orci erat eu massa. Sed sit amet nulla ipsum. Donec felis mauris, vulputate sed tempor at, aliquam a ligula. Pellentesque non pulvinar nisi.</p>
-                            </div>
-                            <div class="tab-pane" id="messages">
-                                <h3>Messages <small>small text</small></h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare dolor, quis ullamcorper ligula sodales at. Nulla tellus elit, varius non commodo eget, mattis vel eros. In sed ornare nulla. Donec consectetur, velit a pharetra ultricies, diam lorem lacinia risus, ac commodo orci erat eu massa. Sed sit amet nulla ipsum. Donec felis mauris, vulputate sed tempor at, aliquam a ligula. Pellentesque non pulvinar nisi.</p>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare dolor.</p>
-                            </div>
+                           
                         </div>
                     </div>
                 </div><!--/span-->
