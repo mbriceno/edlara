8a33833aa460eee2840d33e0fa1c222adef325ae
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-10-02 12:20:11 +0530

    updating sidebar widget.
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- public/themes/dashboard/widgets/sidebar.blade.php
+++ public/themes/dashboard/widgets/sidebar.blade.php
@@ -1,34 +1,34 @@
                 <div class="well nav-collapse sidebar-nav">
                     <ul class="nav nav-tabs nav-stacked main-menu">
-                        <li class="nav-header hidden-tablet">Dashboard</li>
+                        <li class="nav-header hidden-sm">Dashboard</li>
                         <li class="">
                             <a href="/">
                                 <i class="icon-home"></i>
-                                <span class="hidden-tablet">  Dashboard</span>
+                                <span class="hidden-sm"> Dashboard</span>
                             </a>
                         </li>
                         <li class="">
                             <a href="/tutorials">
-                                <i class="icon-edit"></i>
-                                <span class="hidden-tablet">  Tutorials</span>
+                                <i class="icon-copy"></i>
+                                <span class="hidden-sm"> Tutorials</span>
                             </a>
                         </li>      
                         <li class="">
                             <a href="/assessments">
-                                <i class="icon-edit"></i>
-                                <span class="hidden-tablet">  Assessments</span>
+                                <i class="icon-file-alt"></i>
+                                <span class="hidden-sm"> Assessments</span>
                             </a>
                         </li> 
                         <li class="">
                             <a class="" href="/exams">
-                                <i class="icon-list-alt"></i>
-                                <span class="hidden-tablet">  Exams</span>
+                                <i class="icon-newspaper"></i>
+                                <span class="hidden-sm"> Exams</span>
                             </a>
                         </li>                       
                         <li class="">
                             <a class="" href="/students">
-                                <i class="icon-user"></i>
-                                <span class="hidden-tablet"> Students</span>
+                                <i class="icon-user-5"></i>
+                                <span class="hidden-sm"> Students</span>
                             </a>
                         </li>
 
@@ -37,30 +37,30 @@
                         $admingroup = Sentry::findGroupByName('admin');
                         if($user->ingroup($admingroup)){
                         ?>
-                        <li class="nav-header hidden-tablet"> Administration</li>
+                        <li class="nav-header hidden-sm"> Administration</li>
                         
                         <li class="">
                             <a class="" href="/settings">
                                 <i class="icon-cog"></i>
-                                <span class="hidden-tablet"> Settings</span>
+                                <span class="hidden-sm"> Settings</span>
                             </a>
                         </li>                        
                         <li class="">
                             <a class="" href="/teachers">
-                                <i class="icon-user"></i>
-                                <span class="hidden-tablet"> Teachers</span>
+                                <i class="icon-user-4"></i>
+                                <span class="hidden-sm"> Teachers</span>
                             </a>
                         </li>
                         <li class="">
                             <a class="" href="/users">
-                                <i class="icon-user"></i>
-                                <span class="hidden-tablet"> Users</span>
+                                <i class="icon-users"></i>
+                                <span class="hidden-sm"> Users</span>
                             </a>
                         </li>
                         <li class="">
                             <a class="" href="/subjects">
                                 <i class="icon-folder-open"></i>
-                                <span class="hidden-tablet"> Subjects</span>
+                                <span class="hidden-sm"> Subjects</span>
                             </a>
                         </li>
                         <?php
