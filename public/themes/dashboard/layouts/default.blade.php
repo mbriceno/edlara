1855ad8b4fbe7e86f17985d582d7d413203a2566
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-10-02 12:40:08 +0530

    including datatables css
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- public/themes/dashboard/layouts/default.blade.php
+++ public/themes/dashboard/layouts/default.blade.php
@@ -14,6 +14,7 @@
 
         <link rel="stylesheet" type="text/css" href="/lib/fontawesome/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="/lib/system/style.css">
+        <link rel="stylesheet" type="text/css" href="/lib/datatables/css/jquery.dataTables.css">
     </head>
     <body>      
     <nav class="navbar navbar-fixed-top navbar-default" role="navigation">
