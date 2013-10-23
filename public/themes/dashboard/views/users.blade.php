8d620fdf956d0c60f1f72b7253cfade0e49314ed
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2013-10-23 07:31:03 +0530

    adding users banning feature.
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- public/themes/dashboard/views/users.blade.php
+++ public/themes/dashboard/views/users.blade.php
@@ -52,26 +52,31 @@ defined('ROOT' )|| die('Restricted Access');
                                         <i class="icon-edit icon-white"></i>  
                                         Edit                                            
                                     </a>
-                                    <a class="btn btn-danger" href="/user/'.$user->id.'/delete">
-                                        <i class="icon-trash icon-white"></i> 
-                                        Delete
-                                    </a>&nbsp;';
+                                    ';
                                     $throttle = Sentry::findThrottlerByUserId($user->id);
 
                                     if($suspended = $throttle->isSuspended())
                                     {
                                        echo '<a class="btn btn-success" href="/user/'.$user->id.'/unsuspend">
-                                        <i class="icon icon-unlocked icon-white"></i> 
-                                        Unsuspend
-                                    </a>';
+                                        <i class="icon icon-unlocked icon-white"></i> Unsuspend</a>&nbsp;';
                                     }
                                     else
                                     {
                                         echo '<a class="btn btn-warning" href="/user/'.$user->id.'/suspend">
-                                        <i class="icon icon-locked icon-white"></i> 
-                                        Suspend
-                                    </a>';
+                                        <i class="icon icon-locked icon-white"></i> Suspend</a>&nbsp;';
                                     }
+                                    if($banned = $throttle->isBanned())
+                                    {
+                                       echo '<a class="btn btn-warning" href="/user/'.$user->id.'/unban">
+                                        <i class="icon icon-unlocked icon-white"></i> Unban</a>&nbsp;';
+                                    }
+                                    else
+                                    {
+                                        echo '<a class="btn btn-danger" href="/user/'.$user->id.'/ban">
+                                        <i class="icon icon-locked icon-white"></i> Ban</a>&nbsp;';
+                                    }
+                                    echo '<a class="btn btn-danger" href="/user/'.$user->id.'/delete">
+                                        <i class="icon-trash icon-white"></i> Delete</a>&nbsp;';
                                 echo '</td>';
 
             					echo "</tr>";
