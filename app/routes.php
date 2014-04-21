94c5a622dc02d5817d4268d1b9453a73286144d8
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2014-04-21 19:29:17 +0530

    Update: 	* uncomment commented error catch pages.
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- app/routes.php
+++ app/routes.php
@@ -316,17 +316,17 @@ Route::get('dash',function(){
 Route::get('/',array('as'=>'home','uses'=>'HomeController@index'));
 
 
-// App::missing(function($exception)
-// {
-//     return Response::view('site.error.404', array(), 404);
-// });
-// App::error(function(Exception $exception)
-// {
-//     Log::error($exception);
-//     return Response::view('site.error.system',array(),500);
-// });
-// App::error(function(Illuminate \ Database \ Eloquent \ ModelNotFoundException $exception)
-// {
-//     Log::error($exception);
-//     return Response::view('site.error.system',array(),500);
-// });
+ App::missing(function($exception)
+ {
+     return Response::view('site.error.404', array(), 404);
+ });
+ App::error(function(Exception $exception)
+ {
+     Log::error($exception);
+     return Response::view('site.error.system',array(),500);
+ });
+ App::error(function(Illuminate \ Database \ Eloquent \ ModelNotFoundException $exception)
+ {
+     Log::error($exception);
+     return Response::view('site.error.system',array(),500);
+ });
